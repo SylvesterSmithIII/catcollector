9c9008b9e4bedafb677227283d94737d90d0d028
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-08-10 12:59:39 -0700

    serve static files

--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,14 @@ import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cat, Toy, Photo
 from .forms import FeedingForm
+
+
 
 # Create your views here.
 def home(request):
@@ -15,12 +21,14 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def cats_index(request):
-  cats = Cat.objects.all()
+  cats = Cat.objects.filter(user=request.user)
   return render(request, 'cats/index.html', {
     'cats': cats
   })
 
+@login_required
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   # First, create a list of the toy ids that the cat DOES have
@@ -35,18 +43,24 @@ def cats_detail(request, cat_id):
     'toys': toys_cat_doesnt_have
   })
 
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
   model = Cat
   fields = ['name', 'breed', 'description', 'age']
 
-class CatUpdate(UpdateView):
+  def form_valid(self, form):
+
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
   model = Cat
   fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
   model = Cat
   success_url = '/cats'
 
+@login_required
 def add_feeding(request, cat_id):
   # create a ModelForm instance using 
   # the data that was submitted in the form
@@ -62,33 +76,35 @@ def add_feeding(request, cat_id):
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys'
 
+@login_required
 def assoc_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.add(toy_id)
   return redirect('detail', cat_id=cat_id)
 
+@login_required
 def unassoc_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.remove(toy_id)
   return redirect('detail', cat_id=cat_id)
 
-
+@login_required
 def add_photo(request, cat_id):
   
   photo_file = request.FILES.get('photo-file', None)
@@ -110,3 +126,27 @@ def add_photo(request, cat_id):
     
 
     return redirect('detail', cat_id=cat_id)
+
+
+def signup(request):
+    error_message = ''
+    # this will be ran if user tries to signup
+    if request.method == 'POST':
+      
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # save user info
+            user = form.save()
+            # log user in
+            login(request, user)
+            # redirect user
+            return redirect('index')
+      
+        else:
+            error_message = 'Invalid sign up - try again'
+    
+    # this will happen when GET method/ browsing to this URL
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+      
